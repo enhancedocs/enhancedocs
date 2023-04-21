eecfe402fbe080bf34619f39773fedfeefa03010
Alw3ys <alw3ys@gmail.com>
2023-04-21 09:19:33 +0200

    feat: chroma db

--- config.py
+++ config.py
@@ -5,14 +5,15 @@ import json
 
 class Config:
     def __init__(self):
+        self.chroma_collection_name = "enhancedocs"
         config_path = "/etc/enhancedocs"
-        data_path = "/data/enhancedocs"
+        self.data_path = "/data/enhancedocs"
         config_file_name = "config.json"
         prompt_template_file_name = "prompt_template.txt"
         config_file_path = config_path + "/" + config_file_name
         prompt_template_file_path = config_path + "/" + prompt_template_file_name
         os.makedirs(config_path, exist_ok=True)
-        os.makedirs(data_path, exist_ok=True)
+        os.makedirs(self.data_path, exist_ok=True)
         if not os.path.exists(config_file_path):
             shutil.copyfile(config_file_name, config_file_path)
         if not os.path.exists(prompt_template_file_path):
@@ -22,5 +23,5 @@ class Config:
             self.docs_base_url = config_json["docs_base_url"]
         with open(prompt_template_file_path, 'r') as f:
             self.prompt_template = f.read()
-        self.vector_index_file_path = data_path + "/vectorstore.index"
-        self.vector_store_file_path = data_path + "/vectorstore.pkl"
+        self.vector_index_file_path = self.data_path + "/vectorstore.index"
+        self.vector_store_file_path = self.data_path + "/vectorstore.pkl"
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-import faiss
-import pickle
 import json
+import shutil
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,20 +8,27 @@ from langchain import OpenAI
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores.faiss import FAISS
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain import PromptTemplate
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.vectorstores import Chroma
 import utils
 from config import Config
-
+import chromadb
 
 config = Config()
+
+chroma_settings = chromadb.config.Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory=config.data_path
+)
+chroma_client = chromadb.Client(chroma_settings)
 
 load_dotenv()
 app = FastAPI()
 
 llm = OpenAI(temperature=0.7, model_name="gpt-3.5-turbo")
+embedding = OpenAIEmbeddings()
 
 origins = ["*"]
 
@@ -46,26 +52,31 @@ async def ingest_endpoint(request: Request, credentials: str = Depends(utils.ver
         line = json.loads(line)
         for chunk in splitter.split_text(line["content"]):
             source_chunks.append(Document(page_content=chunk, metadata={"source": line["source"]}))
-    store = FAISS.from_documents(source_chunks, OpenAIEmbeddings())
-    faiss.write_index(store.index, config.vector_index_file_path)
-    store.index = None
-    with open(config.vector_store_file_path, "wb") as f:
-        pickle.dump(store, f)
+    try:
+        chroma_client.delete_collection(config.chroma_collection_name)
+        chroma_client.persist()
+    except IndexError:
+        pass
+    Chroma.from_documents(
+        documents=source_chunks,
+        embedding=embedding,
+        persist_directory=config.data_path,
+        collection_name=config.chroma_collection_name
+    )
     return {"message": "Data ingested successfully"}
 
 
 @app.get("/ask")
 def ask_endpoint(question: str, credentials: str = Depends(utils.verify_access_token)):
-    if not os.path.exists(config.vector_index_file_path) or not os.path.exists(config.vector_store_file_path):
+    if not os.listdir(config.data_path):
         raise HTTPException(status_code=404, detail="No data found. Ingest data using "
                                                     "https://github.com/enhancedocs/cli or the API directly")
-    index = faiss.read_index(config.vector_index_file_path)
-    with open(config.vector_store_file_path, "rb") as f:
-        store = pickle.load(f)
-    store.index = index
-    prompt = PromptTemplate(
-        template=config.prompt_template, input_variables=["summaries", "question"]
+    store = Chroma(
+        persist_directory=config.data_path,
+        embedding_function=embedding,
+        collection_name=config.chroma_collection_name
     )
+    prompt = PromptTemplate(template=config.prompt_template, input_variables=["summaries", "question"])
     qa_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=prompt)
     chain = RetrievalQAWithSourcesChain(
         combine_documents_chain=qa_chain,
