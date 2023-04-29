b554b7d52916915922af7febc58d934e2eea2754
Alw3ys <alw3ys@gmail.com>
2023-04-29 22:17:08 +0200

    chore: bring back get endpoint

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ async def ingest_endpoint(request: Request, credentials: str = Depends(utils.ver
 
 class AskModel(BaseModel):
     question: str
-    history: Optional[List[str]] = None
+    history: List[str]
 
 
 @app.post("/ask")
@@ -79,29 +79,39 @@ def ask_endpoint(body: AskModel, credentials: str = Depends(utils.verify_access_
         raise HTTPException(status_code=404, detail="No data found. Ingest data using "
                                                     "https://github.com/enhancedocs/cli or the API directly")
     store = utils.get_vector_store(config)
-    if body.history is not None:
-        question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
-        doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
-        chain = ConversationalRetrievalChain(
-            combine_docs_chain=doc_chain,
-            retriever=store.as_retriever(),
-            question_generator=question_generator,
-            get_chat_history=utils.get_chat_history,
-            return_source_documents=True
-        )
-        result = chain(
-            {"question": body.question, "project_name": config.project_name, "chat_history": body.history},
-            return_only_outputs=True
-        )
-    else:
-        qa_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
-        chain = RetrievalQAWithSourcesChain(
-            combine_documents_chain=qa_chain,
-            retriever=store.as_retriever(),
-            return_source_documents=True
-        )
-        result = chain({"question": body.question, "project_name": config.project_name}, return_only_outputs=True)
+    question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
+    doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
+    chain = ConversationalRetrievalChain(
+        combine_docs_chain=doc_chain,
+        retriever=store.as_retriever(),
+        question_generator=question_generator,
+        get_chat_history=utils.get_chat_history,
+        return_source_documents=True
+    )
+    result = chain(
+        {"question": body.question, "project_name": config.project_name, "chat_history": body.history},
+        return_only_outputs=True
+    )
+    sources = []
+    for document in result['source_documents']:
+        if utils.docusaurus_source_filter(document):
+            sources.append(utils.format_docusaurus_source(document.metadata["source"], config.docs_base_url))
+    return {"answer": result["answer"], "sources": sources}
 
+
+@app.get("/ask")
+def ask_endpoint(question: str, credentials: str = Depends(utils.verify_access_token)):
+    if utils.is_db_empty(config):
+        raise HTTPException(status_code=404, detail="No data found. Ingest data using "
+                                                    "https://github.com/enhancedocs/cli or the API directly")
+    store = utils.get_vector_store(config)
+    qa_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
+    chain = RetrievalQAWithSourcesChain(
+        combine_documents_chain=qa_chain,
+        retriever=store.as_retriever(),
+        return_source_documents=True
+    )
+    result = chain({"question": question, "project_name": config.project_name}, return_only_outputs=True)
     sources = []
     for document in result['source_documents']:
         if utils.docusaurus_source_filter(document):
