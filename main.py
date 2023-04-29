b0a24c1b5446851d66b8b5e7e8e8c6e4ef39bdd7
Alw3ys <alw3ys@gmail.com>
2023-04-29 10:33:05 +0200

    feat: add support to ingest chat history

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import faiss
 import pickle
 
 from dotenv import load_dotenv
+from typing import Optional, List
 from fastapi import Depends, FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.chat_models import ChatOpenAI
@@ -12,6 +13,9 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.faiss import FAISS
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT)
+from langchain.chains.llm import LLMChain
+from langchain.chains import ConversationalRetrievalChain
 
 import utils
 from config import Config
@@ -63,19 +67,39 @@ async def ingest_endpoint(request: Request, credentials: str = Depends(utils.ver
     return {"message": "Data ingested successfully"}
 
 
-@app.get("/ask")
-def ask_endpoint(question: str, credentials: str = Depends(utils.verify_access_token)):
+@app.post("/ask")
+def ask_endpoint(
+        question: str,
+        history: Optional[List[str]] = None,
+        credentials: str = Depends(utils.verify_access_token)
+):
     if utils.is_db_empty(config):
         raise HTTPException(status_code=404, detail="No data found. Ingest data using "
                                                     "https://github.com/enhancedocs/cli or the API directly")
     store = utils.get_vector_store(config)
-    qa_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
-    chain = RetrievalQAWithSourcesChain(
-        combine_documents_chain=qa_chain,
-        retriever=store.as_retriever(),
-        return_source_documents=True
-    )
-    result = chain({"question": question, "project_name": config.project_name}, return_only_outputs=True)
+    if history is not None:
+        question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
+        doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
+        chain = ConversationalRetrievalChain(
+            combine_docs_chain=doc_chain,
+            retriever=store.as_retriever(),
+            question_generator=question_generator,
+            get_chat_history=utils.get_chat_history,
+            return_source_documents=True
+        )
+        result = chain(
+            {"question": question, "project_name": config.project_name, "chat_history": history},
+            return_only_outputs=True
+        )
+    else:
+        qa_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
+        chain = RetrievalQAWithSourcesChain(
+            combine_documents_chain=qa_chain,
+            retriever=store.as_retriever(),
+            return_source_documents=True
+        )
+        result = chain({"question": question, "project_name": config.project_name}, return_only_outputs=True)
+
     sources = []
     for document in result['source_documents']:
         if utils.docusaurus_source_filter(document):
