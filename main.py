0847454b5a0b46e41ee3a4c9d7f2ca97c74593d8
Alw3ys <alw3ys@gmail.com>
2023-04-29 16:58:44 +0200

    provide body as class

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT)
 from langchain.chains.llm import LLMChain
 from langchain.chains import ConversationalRetrievalChain
+from pydantic import BaseModel
 
 import utils
 from config import Config
@@ -67,17 +68,18 @@ async def ingest_endpoint(request: Request, credentials: str = Depends(utils.ver
     return {"message": "Data ingested successfully"}
 
 
+class AskModel(BaseModel):
+    question: str
+    history: Optional[List[str]] = None
+
+
 @app.post("/ask")
-def ask_endpoint(
-        question: str,
-        history: Optional[List[str]] = None,
-        credentials: str = Depends(utils.verify_access_token)
-):
+def ask_endpoint(body: AskModel, credentials: str = Depends(utils.verify_access_token)):
     if utils.is_db_empty(config):
         raise HTTPException(status_code=404, detail="No data found. Ingest data using "
                                                     "https://github.com/enhancedocs/cli or the API directly")
     store = utils.get_vector_store(config)
-    if history is not None:
+    if body.history is not None:
         question_generator = LLMChain(llm=llm, prompt=CONDENSE_QUESTION_PROMPT)
         doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff", prompt=config.prompt)
         chain = ConversationalRetrievalChain(
@@ -88,7 +90,7 @@ def ask_endpoint(
             return_source_documents=True
         )
         result = chain(
-            {"question": question, "project_name": config.project_name, "chat_history": history},
+            {"question": body.question, "project_name": config.project_name, "chat_history": body.history},
             return_only_outputs=True
         )
     else:
@@ -98,7 +100,7 @@ def ask_endpoint(
             retriever=store.as_retriever(),
             return_source_documents=True
         )
-        result = chain({"question": question, "project_name": config.project_name}, return_only_outputs=True)
+        result = chain({"question": body.question, "project_name": config.project_name}, return_only_outputs=True)
 
     sources = []
     for document in result['source_documents']:
